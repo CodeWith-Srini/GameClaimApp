25a0d1f7273efe49fad48b5939ccfb46353710ff
srinibuson <srinivas04112@gmail.com>
2026-05-19 17:44:20 +0530

    fixes

--- lib/Pages/Dashboard/widget/profile.dart
+++ lib/Pages/Dashboard/widget/profile.dart
@@ -1,12 +1,7 @@
-import 'package:Claimit_app/Constant/constantroute.dart';
 import 'package:Claimit_app/Constant/constantvalue.dart';
-import 'package:Claimit_app/Constant/helper.dart';
 import 'package:Claimit_app/Controller/dashboardcrtl.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:glass_kit/glass_kit.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:lottie/lottie.dart';
 import 'package:Claimit_app/Constant/screens.dart';
 import 'package:provider/provider.dart';
 
@@ -30,56 +25,6 @@ class _SettingPageState extends State<SettingPage> {
           ),
           child: Column(
             children: [
-              // SizedBox(height: Screens.padingHeight(context) * 0.03),
-              // Row(
-              //   children: [
-              //     ClipRRect(
-              //       borderRadius: BorderRadius.circular(10),
-              //       child: GlassContainer.clearGlass(
-              //         height: Screens.padingHeight(context) * 0.07,
-              //         width: Screens.width(context) * 0.15,
-              //         borderRadius: BorderRadius.circular(10),
-              //         blur: 5,
-              //         color: Colors.white.withOpacity(0.1),
-              //         borderWidth: 0.5,
-              //         elevation: 10,
-              //         child: Container(
-              //           padding: EdgeInsets.only(
-              //             bottom: Screens.padingHeight(context) * 0.01,
-              //           ),
-              //           decoration: BoxDecoration(
-              //             color: Colors.white.withOpacity(0.2),
-              //             borderRadius: BorderRadius.circular(10),
-              //           ),
-              //           child: Lottie.asset('Assets/user.json'),
-              //         ),
-              //       ),
-              //     ),
-
-              //     SizedBox(width: Screens.width(context) * 0.02),
-              //     Column(
-              //       crossAxisAlignment: CrossAxisAlignment.start,
-              //       children: [
-              //         Text(
-              //           'Srinivas',
-              //           style: theme.textTheme.bodyMedium!.copyWith(
-              //             color: Colors.white,
-              //             fontFamily: GoogleFonts.exo2().fontFamily,
-              //             letterSpacing: 1,
-              //           ),
-              //         ),
-              //         Text(
-              //           'Indie Flutter Developer',
-              //           style: theme.textTheme.bodyMedium!.copyWith(
-              //             color: Colors.white,
-              //             fontFamily: GoogleFonts.exo2().fontFamily,
-              //             letterSpacing: 1,
-              //           ),
-              //         ),
-              //       ],
-              //     ),
-              //   ],
-              // ),
               SizedBox(height: Screens.padingHeight(context) * 0.03),
               Row(
                 children: [
